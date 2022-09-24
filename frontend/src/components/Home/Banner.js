900c18c86baad82926922ce9775af6de9f488e55
S7uXN37 <S7uXN37@users.noreply.github.com>
2022-09-24 12:16:11 +0200

    Frontend Search Functions

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,35 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
+  const changeTitle = (ev) => {
+    ev.preventDefault();
+    const title = ev.target.value.length > 2 ? ev.target.value : "";
+
+    props.onSearchInput(
+      title,
+      (page) => agent.Items.byTitle(title, page),
+      agent.Items.byTitle(title)
+    );
+  };
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <div style={{position: "relative", width:"40%", display: "inline-block", marginLeft: "1em", marginRight: "1em" }}>
+            <input
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="What is it that you truly desire?"
+              
+              onChange={changeTitle}
+            />
+            <i class="ion-search" style={{position: "absolute", right: 0, top: 0, color: "lightgray", transform: "translate(-65%, 25%)", scale: "2"}} /> 
+          </div>
           <span> the cool stuff.</span>
         </div>
       </div>
